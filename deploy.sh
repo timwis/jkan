9d5efb65d555c3b302b151409503cddabbf3c64a
timwis <tim@timwis.com>
2016-02-17 21:32:58 -0500

    change deploy.sh hashbang, just echo test

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,2 @@
-#!/bin/bash
-echo Hello, World! && \
-set -e && \
-jekyll build && \
-cd ./_site && \
-remote_repo=${GITHUB_URL:-`git config remote.origin.url`} && \
-remote_branch="gh-pages" && \
-git init && \
-git config user.name "Travis CI" && \
-git config user.email "nobody@nobody.org" && \
-git add . && \
-git commit -m'build' && \
-git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
-rm -fr .git && \
-cd ../
+#!/usr/bin/env bash
+echo Hello
