98c8c6450a99ef0b45844fc5525558a7e43146f7
timwis <tim@timwis.com>
2016-02-17 19:34:00 -0500

    add deploy & travis config

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e && \
+cd ./_site && \
+remote_repo=${GITHUB_URL:-`git config remote.origin.url`} && \
+remote_branch="gh-pages" && \
+git init && \
+git config user.name "Travis CI" && \
+git config user.email "nobody@nobody.org" && \
+git add . && \
+git commit -m'build' && \
+git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
+rm -fr .git && \
+cd ../
