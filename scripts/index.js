a7818bebbeb0961a5721c5c2f64aa92bfeb4a6b4
timwis <tim@timwis.com>
2016-02-20 20:46:22 -0500

    remove deparam, dataset filters javascript

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-var queryString = window.location.search.substring(1)
-var query = deparam(queryString)
-
-if (query.category) {
-  $('dataset:not([data-category="' + query.category + '"])').hide()
-}
