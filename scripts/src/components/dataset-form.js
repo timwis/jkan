3ffdbdea31c18b2d0d0db3830cf9c3b2a10d8be9
pezholio <pezholio@gmail.com>
2016-04-07 12:05:06 +0100

    Add license field
    The name is populated automatically

--- scripts/src/components/dataset-form.js
+++ scripts/src/components/dataset-form.js
@@ -4,10 +4,26 @@ import 'select2'
 export default class {
   constructor (opts) {
     const elements = {
-      select2: $('.select2', opts.el)
+      select2: $('.select2', opts.el),
+      license: $('#license', opts.el)
     }
 
     // Initialize select2 plugin
     elements.select2.select2()
+
+    elements.license.on('change', function (e) {
+     var selected = $('#license option:selected')
+     var license = selected.val();
+     var name = selected.text()
+     if (license == 'other') {
+       $('#license_url-wrapper').removeClass('hidden')
+       $('#license_name-wrapper').removeClass('hidden')
+     } else {
+       $('#license_url-wrapper').addClass('hidden')
+       $('#license_name-wrapper').addClass('hidden')
+       $('#license_url').val(license)
+       $('#license_name').val(name)
+     }
+   })
   }
 }
