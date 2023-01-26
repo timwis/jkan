34c9dba0e5b5e93b7057b60fe78a2ddf7de8f4a0
timwis <tim@timwis.com>
2023-01-26 07:33:03 +0000

    Save netlify branch to site.branch, and use that in editor config, defaulting to site.github branch This is necessary because `site.github.source.branch` refers to the upstream repo's branch, not the fork. See #227.

--- netlify/plugins/netlify-jekyll-metadata/index.js
+++ netlify/plugins/netlify-jekyll-metadata/index.js
@@ -5,8 +5,9 @@ export const onPreBuild = async function () {
   const siteUrl = process.env.URL
   const repoUrl = new URL(process.env.REPOSITORY_URL || '')
   const repoNameWithOwner = repoUrl.pathname.replace(/^\/+/, '')
+  const branch = process.env.BRANCH
   
   const fileName = '_config_override.yml'
-  const fileContents = `repository: ${repoNameWithOwner}\nurl: ${siteUrl}\nbaseurl: ""`
+  const fileContents = `repository: ${repoNameWithOwner}\nurl: ${siteUrl}\nbranch: ${branch}\nbaseurl: ""`
   return fs.promises.writeFile(fileName, fileContents)
 }
