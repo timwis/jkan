f89fac133a3680342a9422e8c975aa0db192f157
Bryan Quigley <bquigley@element84.com>
2023-03-27 14:18:56 -0700

    Several UI Tweaks - categories, but mostly datasets page
     * Drop old Add-categories linl
     * UI: Sidebar: Increase spacing between Categories and Organizations
     * The original suggestion was Change count pill for selected category and X button to white background
       with Tim's suggestion moved to bg-light text-dark.  Also changed in datasets.html to use primary blue.
     * UI: Results: Increase spacing to make list more legible. I just added top spacing to datasets to the separator is more in the middle.

--- scripts/src/templates/list-group-item.js
+++ scripts/src/templates/list-group-item.js
@@ -2,8 +2,9 @@ export default (data) => (
 `<a href="${data.url}" class="list-group-item${data.selected ? ' active ' : ''} list-group-item-action d-flex justify-content-between align-items-center">
   ${data.title}
   <span>
-    ${data.selected ? '<span class="badge bg-secondary rounded-pill"><i class="fa fa-times"></i></span>' : ''}
-    <span class="badge bg-secondary rounded-pill">${data.count}</span>
+    ${data.selected ? '<span class="badge bg-light text-dark rounded-pill"><i class="fa fa-times"></i></span>' : ''}
+    ${data.selected ? '<span class="badge bg-light text-dark rounded-pill">' : '<span class="badge bg-primary rounded-pill">'}
+      ${data.count}</span>
   </span>
 </a>`
 )
